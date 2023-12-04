177dcbe70364ba2cd9dcd268020e73c8408bfe1d
d-saraki <abiola_saraki@live.co.uk>
2023-12-04 06:29:34 +0000

    add models to admin view

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import BloodTestResults
-from .models import User
+from .models import BloodTestResults, CustomToken, Lab, User
 
 admin.site.register(User, UserAdmin)
 
 
 @admin.register(BloodTestResults)
 class BloodTestResultAdmin(admin.ModelAdmin):
-    list_display = ["user", "timestamp"]
+    list_display = ("user", "timestamp", "results", "ready", "lab")
+
+
+@admin.register(Lab)
+class LabAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "address",
+        "address_2",
+        "city",
+        "post_code",
+        "country",
+        "email",
+        "number",
+    )
+
+
+@admin.register(CustomToken)
+class CustomTokenAdmin(admin.ModelAdmin):
+    list_display = (
+        "key",
+        "user",
+        "name",
+    )
